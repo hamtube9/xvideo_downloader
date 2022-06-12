0478cfbcd82d11e9e7f4dd15cc142a64987344dc
hamtube1996 <haipham@edutalkvn.com>
2022-06-12 15:53:57 +0700

    update

--- videodownloader/lib/views/download_view.dart
+++ videodownloader/lib/views/download_view.dart
@@ -430,7 +430,7 @@ class _DownloadViewState extends State<DownloadView> {
                             toast(context, 'Wait for finish download');
                             return;
                           }
-                          bloc!.getUrlDownload("https://www.xvideos.com/video35033155/beautiful_japanesegirl_-_nanairo.co");
+                          bloc!.getUrlDownload(_urlController!.text);
                           _urlController!.clear();
                         },
                         child: Container(
